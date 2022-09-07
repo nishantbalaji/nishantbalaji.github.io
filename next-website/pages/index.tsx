cfefcee199f7e4c88e6473bdde9d3934a4d6f50f
Nishant Balaji <nishantb1130@gmail.com>
2022-09-07 01:54:11 -0700

    cleanup and finalize

--- next-website/pages/index.tsx
+++ next-website/pages/index.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 import HomeSlide from './homeSlide'
 
 const Home: NextPage = () => {
   return (
     <div>
+      <Head >
+        <title>nishant balaji</title>
+      </Head>
       <HomeSlide />
     </div>
   )
